91784323d382c79b6b7710d04106809e34f4216e
xiaodi007 <334830452@qq.com>
2020-01-13 21:45:37 +0800

    1.add zhuge demo 2.add selector

--- basics/jd_demo.js
+++ basics/jd_demo.js
@@ -1,23 +1,28 @@
-const puppeteer = require('./node_modules/puppeteer-core');
+const puppeteer = require('puppeteer-core');
 const fs = require('fs');
 
-const preloadFile = fs.readFileSync('./basics/js_injection.js', 'utf8');
+const preloadFile = fs.readFileSync('js_injection.js', 'utf8');
 
-const userAgent = 'Mozilla/5.0 (X11; Linux x86_64)' + 'Baiduspider' +
-    'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36';
-const viewport = {width: 1920, height: 1080,}
+const userAgent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)' +
+    'AppleWebKit/537.36 (KHTML, like Gec            ko) Chrome/75.0.3770.100 Safari/537.36';
+const viewport = {width: 1920, height: 1080,};
 
 (async () => {
     // connect chrome docker
-    const browser = await puppeteer.connect({ browserWSEndpoint: 'ws://182.61.30.150:9000' });
+    const browser = await puppeteer.connect({ browserWSEndpoint: 'ws://119.23.255.159:9000' });
     // init new page
     const page = await browser.newPage();
     // set windows height width
-    await page.setViewport(viewport);
+    // await page.setViewport(viewport);
     await page.setUserAgent(userAgent);
     // inject js
     // await page.evaluateOnNewDocument(preloadFile);
-    await page.goto('https://item.jd.com/4148692.html');
-    await page.screenshot({path: './screenshot/jd_without.png'});
+    await page.goto('http://km.xiaoqu.zhuge.com/', {waitUntil: 'networkidle2'});
+    console.log("gotp page")
+    const name = await page.$$eval("#listTableBox > li",
+            el => { return el.map(anchor => anchor.innerHTML) });
+    // await page.goto('https://blog.csdn.net/qq_24448899/article/details/78083319');
+    // await page.screenshot({path: 'jd_without.png', fullPage: true,});
+    console.log(name)
     await browser.close();
 })();
